363e25064ee0274c83c600446072f740508afcef
Misha Berezin <berezinmv@vtblife.ru>
2020-11-12 11:37:59 +0300

    up

--- scripts/updater.js
+++ scripts/updater.js
@@ -7,9 +7,12 @@ chrome.storage.sync.get('version', async function ({ version }) {
 
     const dkp = await import('./dkp.js');
 
+    console.log(dkp);
+
     chrome.storage.sync.set({
         version: CURRENT_VERSION,
         scripts: [
             dkp
-        ]});
+        ]
+    });
 });
