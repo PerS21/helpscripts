0c1becaca8dd5555ce540c9f5edfe505163844e5
perepelkinsr <perepelkinsr@m2.ru>
2020-11-15 16:23:30 +0300

    Start

--- scripts/updater.js
+++ scripts/updater.js
@@ -1,4 +1,4 @@
-const CURRENT_VERSION = '2020.11.12-5';
+const CURRENT_VERSION = '2020.11.12-6';
 
 chrome.storage.local.get('version', async function ({ version }) {
     if(version === CURRENT_VERSION) {
